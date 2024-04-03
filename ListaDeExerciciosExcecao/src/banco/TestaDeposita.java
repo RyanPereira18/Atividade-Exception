edb0818f545967805add8e4ba1a0460479959f25
RyanPereira18 <103114440+RyanPereira18@users.noreply.github.com>
2024-04-03 10:34:10 -0300

    Add files via upload

---
+++ ListaDeExerciciosExcecao/src/banco/TestaDeposita.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco;
+
+
+public class TestaDeposita {
+    public static void main(String[] args) {
+        Conta cp = new ContaPoupanca();
+        
+        try {
+            cp.deposita(-100);
+        }catch(IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
